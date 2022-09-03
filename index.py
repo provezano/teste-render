8c21677194c934d9a0028914cf99eb492ee0ea69
Felipe Provezano Coutinho <provezano@gmail.com>
2022-09-02 21:20:42 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__ == 'main':
+if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
