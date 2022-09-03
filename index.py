0c8b36e8eb984f4ec2b127e101cd25ad2349a970
Felipe Provezano Coutinho <provezano@gmail.com>
2022-09-02 21:19:38 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == 'main':
+    port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=port)
